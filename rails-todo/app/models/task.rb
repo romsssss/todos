dea5de08e4b30fc6fee1a8add87175a47ac88d41
Romain Vigo Benia <romain.vigobenia@gmail.com>
2023-01-12 17:53:26 +0100

    fix(rails): fix rubocop offense

--- rails-todo/app/models/task.rb
+++ rails-todo/app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :title, :presence => true
+  validates :title, presence: true
 
   scope :active, -> { where(completed_at: nil) }
   scope :completed, -> { where.not(completed_at: nil) }
