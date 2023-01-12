a40e996ada778aa714356d2ea3fd5e29dd662f2f
Romain Vigo Benia <romain.vigobenia@gmail.com>
2023-01-12 17:34:14 +0100

    chore(rails): setup end-to-end tests runner in a Github Action (#6)
    * chore(rails): setup end-to-end tests runner in a Github Action
    
    * chore(rails): setup end-to-end tests runner in a Github Action (1)

--- rails-todo/test/application_system_test_case.rb
+++ rails-todo/test/application_system_test_case.rb
@@ -1,5 +1,9 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV.fetch('HEADLESS_CHROME', false)
+    driven_by :selenium, using: :headless_chrome
+  else
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
 end
